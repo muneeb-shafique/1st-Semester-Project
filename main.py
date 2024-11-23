0713c543002c72806cb3b9b030a40d0ce1490c94
Muneeb <muneebshafiq512@gmail.com>
2024-11-23 19:45:23 +0500

    Defined CreateResult Function to Create Result in Resultcards Directory. The DeleteResult Function is defined to Delete the Result Folder. These functions are written in such a way that they handle every kind of unexpected Input.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 # This function is for presentation to make program Visually Attractive
 def Presentation(n):
     os.system("cls")  # To Clear the Output Screen
+    os.system("color 0a") # To Change Text color to Green
     if n=="main":
         print("-----------------------------------------------")
         print("| Welcome to Students Grade Management System |")
@@ -14,10 +15,40 @@ def Presentation(n):
         print("5. Delete Result Cards")
         print("6. About")
         print("7. Exit")
+    elif n=="create":
+        print("-----------------------------------------------")
+        print("|               Create Results                |")
+        print("-----------------------------------------------")
+    elif n=="delete":
+        print("-----------------------------------------------")
+        print("|               Delete Results                |")
+        print("-----------------------------------------------")
+    elif n=="view":
+        print("-----------------------------------------------")
+        print("|                 View Results                |")
+        print("-----------------------------------------------")
+    elif n=="add":
+        print("-----------------------------------------------")
+        print("|                  Add Details                |")
+        print("-----------------------------------------------")
+    elif n=="remove":
+        print("-----------------------------------------------")
+        print("|               Remove Results                |")
+        print("-----------------------------------------------")
 
+# To Create Results and Redirect to AddDetails() Function.
 def CreateResult():
-    pass
+    Presentation("create")
+    result_name = input("Enter Result Name: ")
+    if not os.path.isdir("Resultcards/"+result_name):
+        os.mkdir("Resultcards/"+result_name)
+        input("Result Created Successfully! Press Enter to add Students Information.")
+        AddDetail()
+    else:
+        input("Result Already Exists! Press Enter Key to go back.")
+        main()
 
+# To View the No. of Results We have created till now.
 def ViewResult():
     pass
 
@@ -26,6 +57,38 @@ def AddDetail():
 
 def RemoveDetail():
     pass
+
+def DeleteResult():
+    count=1
+    Presentation("delete")
+    print("Result Cards")
+    results = os.listdir("Resultcards/")
+    if len(results)!=0:
+        for result in results:
+            print(str(count)+") "+result)
+            count+=1
+        num = input("Index: ")
+        
+        if num != "" and num.isdigit() and int(num) < count:
+            index=int(num)-1
+            os.system("cls")
+            print("Are you sure you want to delete",results[index],"results permanently? (Y/N)")
+            sure=input()
+            os.system("cls")
+            if sure.upper() =='Y' or sure.upper()=="YES": 
+                os.rmdir("Resultcards/"+results[index])
+                print("Result Removed Successfully!")
+            elif sure.upper() =='N' or sure.upper()=="NO":
+                print("Deletion of",results[index],"is canceled.")
+            else:
+                print("Invalid Input! Press Enter Key go to back.")
+        else:
+            os.system("cls")
+            print("Please Enter Valid Index.")
+    else:
+        print("No Resultcards Available")
+    input()
+    main()
 
 def About():
     pass
@@ -42,6 +105,8 @@ def main():
         AddDetail()
     elif num=='4':
         RemoveDetail()
+    elif num=='5':
+        DeleteResult()
     elif num=='6':
         About()
     elif num=='7':
